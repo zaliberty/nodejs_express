aa2282b459322059d4a7bc8aab2fb73281870101
zaliberty <vs@naviworldcorp.com>
2018-04-07 14:43:09 +0300

    init code to chapter 05

---
+++ ch_05/meadowlark/site/meadowlark.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var fortune = require('./lib/fortune')
+
+
+var app = express();
+
+// Установка механизма представления handlebars
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about', { fortune: fortune.getFortune() });
+});
+
+
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost' + app.get('port') + ': нажмите Ctrl+C для завершения');
+});
