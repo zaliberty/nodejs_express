a33bb345d4dc81a3f4b77218b68ccc0c95b275eb
zaliberty <vs@naviworldcorp.com>
2018-04-17 09:21:52 +0300

    fix tests in projects

--- ch_05/meadowlark/site/meadowlark.js
+++ ch_05/meadowlark/site/meadowlark.js
@@ -14,6 +14,13 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+
 app.get('/', function(req, res){
     res.render('home');
 });
